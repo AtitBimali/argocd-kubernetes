b4c5659d29a565fa500028ea5fe99ec98a44631c
AtitBimali <atitbimali10@gmail.com>
2024-02-07 22:35:56 +0545

    feat: update > trigger flow

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! This is my Flask app.'
+    return 'Hello, World! This is my Flask app. update'
 
 # if __name__ == '__main__':
 #     app.run(debug=True, host='0.0.0.0', port=5000)
